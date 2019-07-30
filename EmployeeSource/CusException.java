da49525f6f67ed4d475ac0a8afd2aaac5c2d24f9
sakthinandhini <sakthi.sekar@atmecs.com>
2019-07-30 19:02:22 +0530

    Custom-Exception

---
+++ EmployeeSource/CusException.java
@@ -0,0 +1,10 @@
+package EmployeeSource;
+
+import javax.imageio.IIOException;
+
+class TestCustomException1 extends Exception {  
+	 public TestCustomException1()
+	 {
+		 super("employee id is not to be null and enter the valid id");
+	 }
+	}  
